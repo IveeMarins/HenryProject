4c58cbc3340df0bb60634796311f2345b671661c
IveeMarins <iveemarins@gmail.com>
2015-02-06 13:42:19 -0200

    Nova Tela Inicial

--- Henry & The Lantern of Fears/Menu.m
+++ Henry & The Lantern of Fears/Menu.m
@@ -12,29 +12,30 @@
 #import "Mundos.h"
 
 @implementation Menu
-
-
+{
+        SKSpriteNode *_lanternInicial;
+}
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size])
     {
-        SKSpriteNode *Background;
-        Background = [SKSpriteNode spriteNodeWithImageNamed: @"startScreenBackground.jpg"];
-        Background.position = CGPointMake(self.scene.frame.size.width *0.5, self.scene.frame.size.height * 0.5);
-        Background.size = CGSizeMake(self.scene.frame.size.width + 50, self.scene.frame.size.height);
-        [Background setScale:1.05];
-        [self addChild:Background];
+        //Adicionando: Background, Start e Lanterna
+        SKSpriteNode *background;
+        background = [SKSpriteNode spriteNodeWithImageNamed: @"telaInicial"];
+        background.position = CGPointMake(self.scene.frame.size.width * 0.5, self.scene.frame.size.height * 0.5);
+        background.size = CGSizeMake(self.scene.frame.size.width , self.scene.frame.size.height);
+        [self addChild:background];
         
-        SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Bodoni 72 Smallcaps"];
-        label.text = @"Start";
-        label.fontSize = 30;
-        label.position = CGPointMake(self.scene.frame.size.width * 0.5 ,self.scene.frame.size.height * 0.5 /*- 180*/);
-        label.name = @"start";
+        _lanternInicial = [SKSpriteNode spriteNodeWithImageNamed:@"lanternInicial"];
+        _lanternInicial.position = CGPointMake(background.frame.size.width * 0.5, background.frame.size.height * 0.5 - 35);
+        [_lanternInicial setScale: 0.7];
+        [self addChild: _lanternInicial];
         
-        [self addChild:label];
-        [self animateWithPulse:label];
-        
-        
-        
+        SKSpriteNode *buttonStart = [SKSpriteNode spriteNodeWithImageNamed:@"buttonStart"];
+        buttonStart.position = CGPointMake(_lanternInicial.position.x,_lanternInicial.position.y - 110);
+        buttonStart.name = @"start";
+        [buttonStart setScale:0.8];
+        [self addChild: buttonStart];
+        [self animateWithPulse: buttonStart];
         
     }
     return self;
@@ -47,15 +48,37 @@
         SKNode *n = [self nodeAtPoint:[touch locationInNode:self]];
         if([n.name isEqualToString:@"start"])
         {
+            NSString *lanternLightEmmiterPath = [[NSBundle mainBundle] pathForResource:@"LanternMagicFirstScreen" ofType:@"sks"];
+            SKEmitterNode *lanternLightEmmitter = [NSKeyedUnarchiver unarchiveObjectWithFile:lanternLightEmmiterPath];
+            
+            lanternLightEmmitter.position = CGPointMake(self.frame.size.width * 0.5, self.frame.size.height * 0.5 - 50);
+            lanternLightEmmitter.name = @"lanternLightParticle";
+            //lanternLightEmmitter.particleScale = ;
+            lanternLightEmmitter.particleLifetime = 100000;
+           
+            //lanternLightEmmitter.particleSize = CGSizeMake(self.frame.size.width, self.frame.size.height);
+            
+            SKLightNode *lanternLight = [[SKLightNode alloc] init];
+            lanternLight.name = @"lanternLight";
+            lanternLight.position = CGPointMake(_lanternInicial.frame.size.width * 0.5, _lanternInicial.frame.size.height *0.5);
+            
+            [lanternLightEmmitter addChild:lanternLight];
+            [self addChild:lanternLightEmmitter];
+            
+            
             NSLog(@"mundou pro mundo");
             
             Mundos *scene = [[Mundos alloc] initWithSize:self.view.bounds.size];
             scene.scaleMode = SKSceneScaleModeAspectFill;
             
+            
+            
             // Present the scene.
-            SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
+            SKTransition *reveal = [SKTransition fadeWithDuration:10];
             [self.view presentScene:scene transition: reveal];
-
+            
+            
+            
         }
     }
 }
@@ -63,8 +86,8 @@
 
 -(void)animateWithPulse:(SKNode *)node
 {
-    SKAction *disappear = [SKAction fadeAlphaTo:0.0 duration:0.3];
-    SKAction *appear = [SKAction fadeAlphaTo:1.0 duration:0.3];
+    SKAction *disappear = [SKAction fadeAlphaTo:0.0 duration:1];
+    SKAction *appear = [SKAction fadeAlphaTo:1.0 duration:1];
     SKAction *pulse = [SKAction sequence:@[disappear,appear]];
     [node runAction:[SKAction repeatActionForever:pulse]];
 }
